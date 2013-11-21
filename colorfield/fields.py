f556ffbf20adc96c67e257e9cf811808c2dd1551
Kevin Richardson <kevin@magically.us>
2013-11-21 11:11:41 -0500

    Add South introspection rules for ColorField

--- colorfield/fields.py
+++ colorfield/fields.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 import re
 
 from django import forms
@@ -9,16 +7,19 @@ from django.core.validators import RegexValidator
 from django.template.loader import render_to_string
 from django.utils.translation import ugettext_lazy as _
 
+
 color_re = re.compile('^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$')
 validate_color = RegexValidator(color_re, _(u'Enter a valid color.'), 'invalid')
+
 
 class ColorWidget(forms.Widget):
     class Media:
         js = [settings.MEDIA_URL + 'colorfield/jscolor.js']
-        
+
     def render(self, name, value, attrs=None):
         return render_to_string('colorfield/color.html', locals())
-        
+
+
 class ColorField(models.CharField):
     default_validators = [validate_color]
 
@@ -30,4 +31,9 @@ class ColorField(models.CharField):
         kwargs['widget'] = ColorWidget
         return super(ColorField, self).formfield(**kwargs)
 
-# vim: et sw=4 sts=4
+try:
+    from south.modelsinspector import add_introspection_rules
+    add_introspection_rules([], ['^colorfield\.fields\.ColorField'])
+except ImportError:
+    pass
+
